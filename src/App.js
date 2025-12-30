80817318c4641e4e5336c2dc6763865bb8c08f43
Aditya <2604aditya@gmail.com>
2025-12-30 16:04:46 +0530

    Add RippleGrid background animation component with WebGL effects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./components/Navbar";
-import { Provider } from "react-redux"; 
+import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Document from "./components/Document";
 import UploadDocument from "./components/UploadDocument";
 import Home from "./components/Home";
@@ -16,59 +16,47 @@ import Dashboard from "./components/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { useAuthInit } from "./hooks/useAuthInit";
-import './App.css';
+import RippleGrid from "./components/RippleGrid"; // 👈 must be default import
+import "./App.css";
 
 function AppContent() {
   useAuthInit();
-  
+
   return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route 
-          path="/dashboard" 
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/documents" 
-          element={
-            <ProtectedRoute>
-              <Document />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/upload" 
-          element={
-            <ProtectedRoute>
-              <UploadDocument />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="/profile" 
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          } 
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
+    <>
+      {/* 🌌 GLOBAL WEBGL BACKGROUND */}
+      <RippleGrid enableRainbow={true}
+    gridColor="#ffffff"
+    rippleIntensity={0.05}
+    gridSize={10}
+    gridThickness={15}
+    mouseInteraction={true}
+    mouseInteractionRadius={1.2}
+    opacity={0.8} />
+
+      {/* FOREGROUND UI */}
+      <div className="relative z-20 min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/documents" element={<ProtectedRoute><Document /></ProtectedRoute>} />
+          <Route path="/upload" element={<ProtectedRoute><UploadDocument /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
-function App() {
+export default function App() {
   return (
     <Provider store={store}>
       <ThemeProvider>
@@ -79,5 +67,3 @@ function App() {
     </Provider>
   );
 }
-
-export default App;
